0497bd89690e5951fd80384f0ec9ad703d1f74f4
Solution Cat Sadik <barthobalok24@gmail.com>
2020-08-17 10:34:39 +0600

    Initial Commit

---
+++ PhotoCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCheck
+//
+//  Created by Solution Cat Sadik on 17/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
